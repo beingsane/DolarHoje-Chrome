6a09870f38c94458a2f16ae346b03f2b52d6092b
Pedro Menezes <pedrojudo@gmail.com>
2014-09-26 19:03:29 -0300

    Limpa as paradinhas e passa a verificar se a cotação é um número

--- src/app.js
+++ src/app.js
@@ -1,62 +1,62 @@
-DolarHoje = {
+App = {
 
-    url: "http://api.dolarhoje.com/",
-    reloadInterval: 600000, // 10 mins in ms
+  url: "http://api.dolarhoje.com/",
+  reloadInterval: 600000, // 10 mins in ms
 
-    badgeBackgroundColor: [178, 183, 32, 255], // yellow
-    clearBadgeTextTimeout: 10000, // 10 secs in ms
+  badgeBackgroundColor: [178, 183, 32, 255], // yellow
+  clearBadgeTextTimeout: 10000, // 10 secs in ms
 
-    changeTooltipText: function(text){ chrome.browserAction.setTitle({title: text}); },
+  changeTooltipText: function(text){ chrome.browserAction.setTitle({title: text}); },
 
-    changeBadgeText: function(text){ chrome.browserAction.setBadgeText({text: text}); },
-    changeBadgeBackgroundColor: function(color){ 
-        chrome.browserAction.setBadgeBackgroundColor({color: color}); 
-    },
+  changeBadgeText: function(text){ chrome.browserAction.setBadgeText({text: text}); },
+  changeBadgeBackgroundColor: function(color){
+    chrome.browserAction.setBadgeBackgroundColor({color: color});
+  },
 
-    loadQuote: function(){
-        var scope = this;
-        var xhr = new XMLHttpRequest();
-    
-        xhr.open("GET", this.url, true);
-        xhr.onreadystatechange = function() {
-          if ((xhr.readyState == 4) && (xhr.status == 200)) {
-              var actualQuote = xhr.responseText;
-              scope.onloadQuote(actualQuote);
-          }
+  loadQuote: function(){
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", this.url, true);
+    xhr.onreadystatechange = function() {
+      if ((xhr.readyState == 4) && (xhr.status == 200)) {
+        var actualQuote = xhr.responseText;
+
+        if (actualQuote.match(/^\d+(?:\,\d*)$/)) {
+          App.onloadQuote(actualQuote);
         }
-        xhr.send();
-    },
-
-    onloadQuote: function(actualQuote){
-        var tooltipText = "US$ 1,00 vale R$ " + actualQuote + " hoje";
-        this.changeTooltipText(tooltipText);
-        this.changeBadgeText(actualQuote);
-    },
-
-    attachBehaviors: function(){
-        var scope = this;
-        chrome.browserAction.onClicked.addListener(function() {
-            window.open("http://dolarhoje.com");
-            scope.trackEvent("BrowserIcon", "click");
-        });
-    },
-
-    startReloadInterval: function(){
-        var scope = this;
-        window.setInterval(function(){ scope.loadQuote(); }, this.reloadInterval);
-    },
-    
-    trackEvent: function(category, action){
-        _gaq.push(['_trackEvent', category, action]);
-    },
-
-    init: function(){
-        this.changeBadgeBackgroundColor(this.badgeBackgroundColor);
-        this.attachBehaviors();
-    
-        this.loadQuote();
-        this.startReloadInterval();
+      }
     }
+    xhr.send();
+  },
+
+  onloadQuote: function(actualQuote){
+    var tooltipText = "US$ 1,00 vale R$ " + actualQuote + " hoje";
+    this.changeTooltipText(tooltipText);
+    this.changeBadgeText(actualQuote);
+  },
+
+  attachBehaviors: function(){
+    chrome.browserAction.onClicked.addListener(function() {
+      window.open("http://dolarhoje.com");
+      App.trackEvent("BrowserIcon", "click");
+    });
+  },
+
+  startReloadInterval: function(){
+    window.setInterval(function(){ App.loadQuote(); }, App.reloadInterval);
+  },
+
+  trackEvent: function(category, action){
+    _gaq.push(['_trackEvent', category, action]);
+  },
+
+  init: function(){
+    this.changeBadgeBackgroundColor(this.badgeBackgroundColor);
+    this.attachBehaviors();
+
+    this.loadQuote();
+    this.startReloadInterval();
+  }
 };
 
-DolarHoje.init();
+App.init();
